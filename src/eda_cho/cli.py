4d7db0f33112e93335ce20381ce9dbf341f615f7
cho6019 <j01040864205@gmail.com>
2025-01-21 11:28:22 +0900

    dev and local test

--- src/eda_cho/cli.py
+++ src/eda_cho/cli.py
@@ -8,9 +8,12 @@ def group_by_count(keyword: str, ascend: bool, rowsize: int):
     f_df = df[df['speech_text'].str.contains(keyword, case=False)]
     rdf = f_df.groupby("president").size().reset_index(name='count')
     sdf = rdf.sort_values(by='count', ascending=ascend).reset_index(drop=True)
-    
-
-    
+    if(rowsize <= 0):
+        sdf = sdf
+    elif(rowsize > len(sdf)):
+        sdf = sdf
+    else:
+        sdf = sdf[0:(rowsize)]
     print(sdf.to_string(index=False))
 
 def entry_point():
