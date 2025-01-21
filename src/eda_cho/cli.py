eaf6db922f0bfd7a98cc737127f21db8ee8dab58
cho6019 <j01040864205@gmail.com>
2025-01-21 11:08:36 +0900

    ready

--- src/eda_cho/cli.py
+++ src/eda_cho/cli.py
@@ -1,12 +1,18 @@
 from president_speech.db.parquet_interpreter import get_parquet_full_path
 import pandas as pd
+import typer
 
-
-def group_by_count(a):
+def group_by_count(keyword: str, ascend: bool, rowsize: int):
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
-    f_df = df[df['speech_text'].str.contains(str(a), case=False)]
+    f_df = df[df['speech_text'].str.contains(keyword, case=False)]
     rdf = f_df.groupby("president").size().reset_index(name='count')
-    sdf = rdf.sort_values(by='count', ascending=False).reset_index(drop=True)
-    return sdf
+    sdf = rdf.sort_values(by='count', ascending=ascend).reset_index(drop=True)
+    
+
+    
+    print(sdf.to_string(index=False))
+
+def entry_point():
+    typer.run(group_by_count)
 
