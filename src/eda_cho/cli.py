b342d70cc158a869c0b1806865e18bc571afa340
cho6019 <j01040864205@gmail.com>
2025-01-21 22:09:26 +0900

    추가 요청사항

--- src/eda_cho/cli.py
+++ src/eda_cho/cli.py
@@ -6,15 +6,19 @@ def group_by_count(keyword: str, ascend: bool=False, rowsize: int=12)-> pd.DataF
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
     f_df = df[df['speech_text'].str.contains(keyword, case=False)]
-    rdf = f_df.groupby("president").size().reset_index(name='count')
-    sdf = rdf.sort_values(by='count', ascending=ascend).reset_index(drop=True)
+    f_df['kc'] = f_df['speech_text'].str.count(keyword)
+    rdf = f_df.groupby("president", as_index=False).agg(count=('speech_text', 'size'),
+                                        keyword_count=('kc', 'sum'))
+    sdf = rdf.sort_values(by=['keyword_count', 'count'], ascending=[ascend, ascend]).reset_index(drop=True)
     if(rowsize <= 0):
         sdf = sdf
     elif(rowsize > len(sdf)):
         sdf = sdf
     else:
-        sdf = sdf[0:(rowsize)]
+        sdf = sdf.head(rowsize)
     return sdf
+
+
 
 def print_group_by_count(keyword: str, ascend: bool=False, rowsize: int=12):
     df = group_by_count(keyword, ascend, rowsize)
