e7d9e5ede9d28259a05b11eb3915006e9a1e3d64
cho6019 <j01040864205@gmail.com>
2025-01-21 16:35:40 +0900

    bug fix

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -1,9 +1,9 @@
-from eda_bam.cli import group_by_count
+from eda_cho.cli import group_by_count
 import pandas as pd
 
 def test_search_exception():
     row_count = 13
-    df = group_by_count(keyword="자유", asc=True, rcnt=row_count)
+    df = group_by_count(keyword="자유", ascend=True, rowsize=row_count)
     
     # assert
     assert isinstance(df, pd.DataFrame)
@@ -15,7 +15,7 @@ def test_정열_및_행수제한():
     is_asc = True
 
     # when
-    df = group_by_count(keyword="자유", asc=is_asc, rcnt=row_count)
+    df = group_by_count(keyword="자유", ascend=is_asc, rowsize=row_count)
     
     # then
     assert isinstance(df, pd.DataFrame)
