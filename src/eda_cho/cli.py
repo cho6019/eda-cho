245a32b5173ad9849106cd97f5f479d8f28f296c
cho6019 <j01040864205@gmail.com>
2025-01-22 14:19:01 +0900

    dictionary question

--- src/eda_cho/cli.py
+++ src/eda_cho/cli.py
@@ -6,10 +6,11 @@ def group_by_count(keyword: str, ascend: bool=False, rowsize: int=12)-> pd.DataF
     data_path = get_parquet_full_path()
     df = pd.read_parquet(data_path)
     f_df = df[df['speech_text'].str.contains(keyword, case=False)]
-    f_df['kc'] = f_df['speech_text'].str.count(keyword)
-    rdf = f_df.groupby("president", as_index=False).agg(count=('speech_text', 'size'),
-                                        keyword_count=('kc', 'sum'))
-    sdf = rdf.sort_values(by=['keyword_count', 'count'], ascending=[ascend, ascend]).reset_index(drop=True)
+    #f_df['kc'] = f_df['speech_text'].str.count(keyword)
+    gdf=f_df.groupby("president").size().reset_index(name="count")
+    #rdf = f_df.groupby("president", as_index=False).agg(count=('speech_text', 'size'), keyword_count=('kc', 'sum'))
+    sdf = gdf.sort_values(by='count', ascending=ascend).reset_index(drop=True)
+    #sdf = rdf.sort_values(by=['keyword_count', 'count'], ascending=[ascend, ascend]).reset_index(drop=True)
     if(rowsize <= 0):
         sdf = sdf
     elif(rowsize > len(sdf)):
--- tests/test_speech.py
+++ tests/test_speech.py
@@ -21,3 +21,45 @@ def test_정열_및_행수제한():
     assert isinstance(df, pd.DataFrame)
     assert df.iloc[0]["president"] == "윤보선"
     assert len(df) == row_count
+
+#def test_wc
+    #given
+    #row_count=3
+    #is_asc = True
+
+    #df=group_by_count(keyword='자유', ascend=is_asc, rowsize=row_count)
+
+def test_default_args():
+
+    df = group_by_count('자유')
+
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 12
+    assert df.iloc[0]['president'] == '박정희'
+    assert df.iloc[0]['count'] == 513
+    assert df.iloc[1, 1] == 438
+
+
+presidents_speeches = {
+    "박정희": 513,
+    "이승만": 438,
+    "노태우": 399,
+    "김대중": 305,
+    "문재인": 275,
+    "김영삼": 274,
+    "이명박": 262,
+    "전두환": 242,
+    "노무현": 230,
+    "박근혜": 111,
+    "최규하": 14,
+    "윤보선": 1
+}
+
+def test_dic():
+    df = group_by_count('자유')
+
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 12
+    for a in presidents_speeches.keys():
+        president_row = df[df['president'] == a]
+        assert president_row.iloc[0]['count'] == presidents_speeches[a]
