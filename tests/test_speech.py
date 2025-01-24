ef2a1d7abc50bb1815d1641b66e4f725e821e013
cho6019 <j01040864205@gmail.com>
2025-01-24 09:34:45 +0900

    누락

--- tests/test_speech.py
+++ tests/test_speech.py
@@ -5,7 +5,7 @@ import pytest
 
 def test_search_exception():
     row_count = 13
-    df = group_by_count(keyword="자유", ascend=True, rowsize=row_count)
+    df = group_by_count(keyword="자유", asc=True, rcnt=row_count)
     
     # assert
     assert isinstance(df, pd.DataFrame)
@@ -27,7 +27,7 @@ def test_정열_및_행수제한():
     is_asc = True
 
     # when
-    df = group_by_count(keyword="자유", ascend=is_asc, rowsize=row_count)
+    df = group_by_count(keyword="자유", asc=is_asc, rcnt=row_count)
     
     # then
     assert isinstance(df, pd.DataFrame)
